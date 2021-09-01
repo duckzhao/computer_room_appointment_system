d6c1c4dd38e614ed9833aa6e93e8739cd28aaedf
1161678627 <1161678627@qq.com>
2021-09-01 23:39:33 +0800

    定义全局的txt文件名宏变量

---
+++ computer_room_appointment_system/globalFile.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// 宏定义所有要用到的文件名，以后调用直接用宏名调用即可，省事，减少拼错率
+#define MANAGER_FILE "manager.txt"
+#define STUDENT_FILE "student.txt"
+#define TEACHER_FILE "teacher.txt"
+#define COMPUTER_FILE "computer.txt"
+#define ORDER_FILE "order.txt"
